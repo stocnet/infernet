9ec294668a317cb7a94c51155246823d7068f991
James Hollway <james.hollway@graduateinstitute.ch>
2026-09-05 07:34:57 +0200

    Improved the loading messages

--- NEWS.md
+++ NEWS.md
@@ -5,12 +5,13 @@
 - Updated `DESCRIPTION`
   - Raised the R minimum to 4.1.0, since the examples use the native pipe
   - Pinned the minimum versions of `{manynet}` and `{netrics}`
+- Updated CONTRIBUTING to document the architecture and the house conventions
+- Added `README.Rmd` and `pkgdown/_pkgdown.yml` for this package
+- Improved the loading messages
 - Updated the Github Actions workflows
   - Added the PR metadata checks for the version bump and the PR title and body
   - Release notes are now taken from the matching `NEWS.md` section
   - Updated the action versions in `prchecks` and `pushrelease`
-- Updated CONTRIBUTING to document the architecture and the house conventions
-- Added `README.Rmd` and `pkgdown/_pkgdown.yml` for this package
 
 
 # infernet 0.1.0
--- R/zzz.R
+++ R/zzz.R
@@ -1,58 +1,41 @@
+# nocov start
+
+# The stocnet version check lives in {migraph}, which loads and checks the whole
+# stack at once. This package is one of the packages it checks, so it does no
+# version check of its own.
 #' @importFrom manynet snet_info
 .onAttach <- function(...) {
 
-  # suppressMessages(suppressPackageStartupMessages(library("manynet", warn.conflicts = FALSE)))
   if (!interactive()) return()
-  
-  # options(manynet_verbosity = getOption("manynet_verbosity", "verbose"))
+
   options(stocnet_theme = getOption("stocnet_theme", "default"))
-  # options(cli.theme = manynet_console_theme())
-  # options(cli.progress_clear = TRUE)
-  
-  # pkgs <- as.data.frame(utils::available.packages(utils::contrib.url(getOption("repos"))))
-  # 
-  # cran_version <- pkgs[pkgs$Package == "manynet","Version"]
 
   local_version <- utils::packageVersion("infernet")
-  manynet::snet_info("You are using {.pkg infernet} version {.version {local_version}}.")
-  old.list <- as.data.frame(utils::old.packages())
-  behind_cran <- "infernet" %in% old.list$Package
-  
+  manynet::snet_info("You are using {.infr infernet} version {.version {local_version}}.")
+
   greet_startup_cli <- function() {
     tips <- c(
-      "i" = "There are lots of ways to contribute to {.pkg infernet} at {.url https://github.com/stocnet/infernet/}.",
-      "i" = "Please let us know any bugs, issues, or feature requests at {.url https://github.com/stocnet/infernet/issues}. It's really helpful!",
-      # "i" = "To eliminate package startup messages, use: `suppressPackageStartupMessages(library({.pkg autograph}))`.",
-      # "i" = "Changing the theme of all your graphs is straightforward with `set_manynet_theme()`",
-      # "i" = "If there are too many messages in the console, run `options(manynet_verbosity = 'quiet')`",
-      "i" = "Visit the website to learn more: {.url https://stocnet.github.io/infernet/}.",
-      "i" = "We recommend the 'Function Overview' page online to discover new analytic opportunities: {.url https://stocnet.github.io/infernet/reference/index.html}.",
-      # "i" = "Star me at {.url https://github.com/users/follow?target=jhollway}.",
-      # "i" = "You can list all the tutorials available in {.pkg manynet} using {.fn run_tute}, and run them too!",
-      "i" = "Discover all the {.emph stocnet} R packages at {.url https://github.com/stocnet/}."
+      "i" = "Share bugs, issues, or feature requests at {.url https://github.com/stocnet/infernet/issues}.",
+      "i" = "If too many messages appear in the console, run {.run base::options(snet_verbosity = 'quiet')}",
+      "i" = "Explore changes since the last version with {.run [news(package = 'infernet')](utils::news(package = 'infernet'))}.",
+      # "i" = "Test any network statistic against a null distribution with {.fn test_random}, {.fn test_configuration}, or {.fn test_permutation}.",
+      # "i" = "Regress a network on nodal and dyadic covariates with {.fn net_regression}.",
+      # "i" = "Write {.code ego()}, {.code alter()}, {.code same()}, {.code dist()}, {.code sim()}, or {.code tertius()} in a formula to build a predictor from a nodal attribute.",
+      "i" = "Speed up a long run with {.code control = list(strategy = 'multisession')}.",
+      # "i" = "Measures to test are in {.tric netrics}; plots of results are in {.auto autograph}.",
+      "i" = "Visit {.url https://stocnet.github.io/infernet/} to learn more.",
+      "i" = "Discover new functions at {.url https://stocnet.github.io/infernet/reference/index.html}.",
+      "i" = "Discover {.emph stocnet} R packages at {.url https://github.com/stocnet/}."
     )
     manynet::snet_info(sample(tips, 1))
   }
 
-  if (interactive()) {
-    if (behind_cran) {
-      msg <- "A new version of infernet is available with bug fixes and new features."
-      packageStartupMessage(msg, "\nWould you like to install it?")
-      if (utils::menu(c("Yes", "No")) == 1) {
-        utils::update.packages("infernet")
-      }
-    } else {
-      greet_startup_cli()
-      # packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
-    }
-  }
+  greet_startup_cli()
 
 }
+
+# nocov end
 
 # Global variables ####
 # defining global variables more centrally
 utils::globalVariables(c(".data"))
-
-
-
-
